4bed1837ef1a1de7994d9d37be232b3dfb44b800
Andy Bryan Julca Tello <julcac137@gmail.com>
2023-12-19 19:44:20 -0500

    first commit

---
+++ set.swift
@@ -0,0 +1,8 @@
+var languages: Set = ["Swift", "Java", "Python"]
+
+print("Initial Set: \(languages)")
+
+// remove Java from a set
+let removedValue = languages.remove("Java")
+
+print("Set after remove(): \(languages)")
